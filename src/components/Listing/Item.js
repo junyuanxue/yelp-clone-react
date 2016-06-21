935cbe206649640275259b4175d136523a4caa09
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-21 17:28:40 +0100

    Displaying stars in rating. All tests green.

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -1,7 +1,7 @@
 import React, { PropTypes as T } from 'react'
 import classnames from 'classnames'
 
-// import Rating from 'components/Rating/Rating'
+import Rating from 'components/Rating/Rating'
 import styles from './styles.module.css'
 
 export class Item extends React.Component {
@@ -13,7 +13,7 @@ export class Item extends React.Component {
           {place.name}
         </h1>
         <span>
-          {place.rating/5}
+          <Rating percentage={place.rating/5} />
         </span>
       </div>
     )
