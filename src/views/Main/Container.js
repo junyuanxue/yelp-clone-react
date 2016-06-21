8ce8cc12dd4fcceae6f2b90801eae911044687f2
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-21 11:54:07 +0100

    created googleApiHelper to search nearby places and passing it into Container. Set state for Container

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
+import {searchNearby} from 'utils/googleApiHelpers'
 
 export class Container extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  }
+
+  onReady(mapProps, map) {
+    const {google} = this.props;
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        console.log(results);
+        console.log(pagination);
+      }).catch((status, result) => {
+        console.log('Error: ' + status + result);
+      })
+  }
+
   render() {
     return (
       <div>
-        Hello from the container
-        <Map google={this.props.google} />
+        <Map
+          onReady={this.onReady.bind(this)}
+          google={this.props.google} />
       </div>
     )
   }
