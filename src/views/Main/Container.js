ff65b74fd58ae447ed6ae90c9497f39f4fad922d
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-21 11:29:22 +0100

    Loaded google map Map component in Container using google-maps-react

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 export class Container extends React.Component {
   render() {
     return (
       <div>
         Hello from the container
+        <Map google={this.props.google} />
       </div>
     )
   }
 }
 
-export default Container;
+export default GoogleApiWrapper({
+  apiKey: __GOOGLE_API_KEY__
+})(Container);
