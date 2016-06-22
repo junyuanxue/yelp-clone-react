65658d490322ee6fb10e9f391010a1febf1faa3a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-22 10:56:59 +0100

    Added markers on the map using Google Map Marker component

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { PropTypes as T } from 'react'
-import GoogleMap from 'google-maps-react'
+import GoogleMap, { Marker } from 'google-maps-react'
 import classnames from 'classnames'
 
 import styles from './styles.module.css'
@@ -7,7 +7,17 @@ import styles from './styles.module.css'
 export class Map extends React.Component {
   static defaultProps = {
     center: {lat: 51.517354, lng: -0.072487}
-  };
+  }
+
+  renderMarkers() {
+    return this.props.places.map(place => {
+      return <Marker
+                key={place.id}
+                name={place.id}
+                place={place}
+                position={place.geometry.location} />
+    })
+  }
 
   render() {
     return (
@@ -15,6 +25,7 @@ export class Map extends React.Component {
         center={this.props.center}
         google={this.props.google}
         className={styles.map} >
+        {this.renderMarkers()}
       </GoogleMap>
     )
   }
